cb05e12e8e2d2b14e8c1d621857b0408074d87e9
Nick Bernard <nick.bernard@hey.com>
2025-06-17 14:45:29 -0700

    Crawl pages recursively following links from root URL

--- main.go
+++ main.go
@@ -2,8 +2,60 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 )
+
+func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
+	fmt.Printf("crawling %s\n", rawCurrentURL)
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		fmt.Println("error parsing base URL: ", err)
+		return
+	}
+	currentURL, err := url.Parse(rawCurrentURL)
+	if err != nil {
+		fmt.Println("error parsing current URL: ", err)
+		return
+	}
+
+	if currentURL.Host != baseURL.Host {
+		fmt.Println("current URL is not on the same host as the base URL")
+		return
+	}
+
+	normalizedCurrentURL, err := normalizeURL(currentURL.String())
+	if err != nil {
+		fmt.Println("error normalizing current URL: ", err)
+		return
+	}
+
+	if pages[normalizedCurrentURL] > 0 {
+		pages[normalizedCurrentURL]++
+		return
+	} else {
+		pages[normalizedCurrentURL] = 1
+	}
+
+	html, err := getHTML(currentURL.String())
+	if err != nil {
+		fmt.Println("error getting HTML: ", err)
+		return
+	}
+
+	fmt.Printf("html for %s\n", normalizedCurrentURL)
+	fmt.Println(html)
+
+	urls, err := getURLsFromHTML(html, currentURL.String())
+	if err != nil {
+		fmt.Println("error getting URLs from HTML: ", err)
+		return
+	}
+
+	for _, url := range urls {
+		crawlPage(rawBaseURL, url, pages)
+	}
+}
 
 func main() {
 	args := os.Args[1:]
@@ -20,12 +72,12 @@ func main() {
 	url := args[0]
 	fmt.Printf("starting crawl of: %s\n", url)
 
-	html, err := getHTML(url)
-	if err != nil {
-		fmt.Printf("error getting HTML: %v\n", err)
-		os.Exit(1)
+	pages := map[string]int{}
+	crawlPage(url, url, pages)
+
+	for page, count := range pages {
+		fmt.Printf("%s: %d\n", page, count)
 	}
 
-	fmt.Printf("%s", html)
 	os.Exit(0)
 }
