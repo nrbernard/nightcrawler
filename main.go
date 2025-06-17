f69afeddffa4fe7ed7e587d04d2b7523c86721f2
Nick Bernard <nick.bernard@hey.com>
2025-06-17 15:35:13 -0700

    Add concurrency

--- main.go
+++ main.go
@@ -4,22 +4,47 @@ import (
 	"fmt"
 	"net/url"
 	"os"
+	"sync"
+	"time"
 )
 
-func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
-	fmt.Printf("crawling %s\n", rawCurrentURL)
-	baseURL, err := url.Parse(rawBaseURL)
-	if err != nil {
-		fmt.Println("error parsing base URL: ", err)
-		return
+type config struct {
+	pages              map[string]int
+	baseURL            *url.URL
+	mu                 *sync.Mutex
+	concurrencyControl chan struct{}
+	wg                 *sync.WaitGroup
+}
+
+func (cfg *config) addPageVisit(normalizedURL string) (isFirst bool) {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+
+	if cfg.pages[normalizedURL] > 0 {
+		cfg.pages[normalizedURL]++
+		return false
+	} else {
+		cfg.pages[normalizedURL] = 1
+		return true
 	}
+}
+
+func (cfg *config) crawlPage(rawCurrentURL string) {
+	cfg.concurrencyControl <- struct{}{}
+	defer func() {
+		<-cfg.concurrencyControl
+		cfg.wg.Done()
+	}()
+
+	fmt.Printf("crawling %s\n", rawCurrentURL)
+
 	currentURL, err := url.Parse(rawCurrentURL)
 	if err != nil {
 		fmt.Println("error parsing current URL: ", err)
 		return
 	}
 
-	if currentURL.Host != baseURL.Host {
+	if currentURL.Host != cfg.baseURL.Host {
 		fmt.Println("current URL is not on the same host as the base URL")
 		return
 	}
@@ -30,11 +55,9 @@ func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
 		return
 	}
 
-	if pages[normalizedCurrentURL] > 0 {
-		pages[normalizedCurrentURL]++
+	isFirst := cfg.addPageVisit(normalizedCurrentURL)
+	if !isFirst {
 		return
-	} else {
-		pages[normalizedCurrentURL] = 1
 	}
 
 	html, err := getHTML(currentURL.String())
@@ -53,11 +76,14 @@ func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
 	}
 
 	for _, url := range urls {
-		crawlPage(rawBaseURL, url, pages)
+		cfg.wg.Add(1)
+		go cfg.crawlPage(url)
 	}
 }
 
 func main() {
+	start := time.Now()
+
 	args := os.Args[1:]
 	if len(args) < 1 {
 		fmt.Println("no website provided")
@@ -69,15 +95,28 @@ func main() {
 		os.Exit(1)
 	}
 
-	url := args[0]
-	fmt.Printf("starting crawl of: %s\n", url)
-
-	pages := map[string]int{}
-	crawlPage(url, url, pages)
-
-	for page, count := range pages {
-		fmt.Printf("%s: %d\n", page, count)
+	baseURL, err := url.Parse(args[0])
+	if err != nil {
+		fmt.Println("error parsing base URL: ", err)
+		os.Exit(1)
 	}
 
+	cfg := config{
+		pages:              map[string]int{},
+		baseURL:            baseURL,
+		mu:                 &sync.Mutex{},
+		concurrencyControl: make(chan struct{}, 10),
+		wg:                 &sync.WaitGroup{},
+	}
+
+	fmt.Printf("starting crawl of: %s\n", baseURL.String())
+
+	cfg.wg.Add(1)
+	cfg.crawlPage(baseURL.String())
+
+	cfg.wg.Wait()
+
+	fmt.Printf("pages crawled: %v\n", len(cfg.pages))
+	fmt.Printf("crawl completed in %v\n", time.Since(start))
 	os.Exit(0)
 }
